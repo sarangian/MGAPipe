51565c637a9efda94ee32e5a3d42eb002fbe93e5
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2020-11-23 16:48:22 +0530

    Add files via upload

---
+++ metagenome.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import luigi
+import sys
+
+from tasks.metagenome.readCleaning.rawReadQC import readqc
+from tasks.metagenome.readCleaning.rawReadQC import rawReadsQC
+from tasks.metagenome.readCleaning.preProcessReads import cleanFastq
+from tasks.metagenome.readCleaning.preProcessReads import cleanReads
+from tasks.metagenome.readCleaning.preProcessReads import filtlong
+
+
+from tasks.metagenome.utility.luigiconfig import configureProject
+
+from tasks.metagenome import metaAssembly
+from tasks.metagenome import genome_binning
+from tasks.metagenome import bin_refinement
+from tasks.metagenome import genome_dereplicate
+from tasks.metagenome import genome_annotation
+from tasks.metagenome import genome_enrichment
+from tasks.metagenome import metagenome_profiling
+
+
+#from tasks.metagenome import meta_profile
+#from tasks.metagenome import metagenome_analysis
+
+#from tasks.annotation import prokaryotic_annotation
+#from tasks.annotation.make_tx_to_gene import makeTx2Gene
+
+
+if __name__ == '__main__':
+
+    luigi.run()
